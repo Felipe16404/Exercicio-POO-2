d76f28d8d832dad34a2fa512b9b30e8cff619171
Felipe16404 <86027320+Felipe16404@users.noreply.github.com>
2021-06-18 22:02:53 -0300

    Update Program.cs

--- ExercicioPOO2/Program.cs
+++ ExercicioPOO2/Program.cs
@@ -9,24 +9,24 @@ namespace VSProject
             // classe Converter converte temperaturas para Celsius ou Fahrenheit
             Converter celsius = new Converter();
             double res1 = celsius.toFahrenheit(50);
-            Console.WriteLine(res1);
+            Console.WriteLine(res1); // 122 F
 
             Converter fahrenheit = new Converter();
             double res2 = fahrenheit.toCelsius(50);
-            Console.WriteLine(res2);
+            Console.WriteLine(res2); // 10 C
 
             // classe Matematica realiza operações simples de adição e multiplicação
             Matematica calculo1 = new Matematica();
             double resultado1 = calculo1.Adicao(5, 10);
-            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado1); // 15
 
             Matematica calculo2 = new Matematica();
             double resultado2 = calculo2.Multiplicacao(5, 10);
-            Console.WriteLine(resultado2);
+            Console.WriteLine(resultado2); // 50
 
             // classe ImparPar diz se o numero inserido é impar ou par
             ImparPar n1 = new ImparPar(7);
-            Console.WriteLine(n1.resultado);
+            Console.WriteLine(n1.resultado); // é impar
         }
     }
 }
