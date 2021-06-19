35398b216520b1a82b0a1b644d8b0ee16da7b4f8
Felipe16404 <86027320+Felipe16404@users.noreply.github.com>
2021-06-18 21:54:08 -0300

    Add files via upload
    C# exercício 02 métodos e classes

---
+++ ExercicioPOO2/ImparPar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VSProject
+{
+    public class ImparPar
+    {
+        public double valor;
+        public string resultado;
+        public ImparPar (double numero)
+        {
+            valor = numero;
+            if (valor % 2 == 0) {
+                resultado = "É par";
+            }
+            else
+            {
+                resultado = "É impar";
+            }
+        }
+    }
+}
---
+++ ExercicioPOO2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace VSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // classe Converter converte temperaturas para Celsius ou Fahrenheit
+            Converter celsius = new Converter();
+            double res1 = celsius.toFahrenheit(50);
+            Console.WriteLine(res1);
+
+            Converter fahrenheit = new Converter();
+            double res2 = fahrenheit.toCelsius(50);
+            Console.WriteLine(res2);
+
+            // classe Matematica realiza operações simples de adição e multiplicação
+            Matematica calculo1 = new Matematica();
+            double resultado1 = calculo1.Adicao(5, 10);
+            Console.WriteLine(resultado1);
+
+            Matematica calculo2 = new Matematica();
+            double resultado2 = calculo2.Multiplicacao(5, 10);
+            Console.WriteLine(resultado2);
+
+            // classe ImparPar diz se o numero inserido é impar ou par
+            ImparPar n1 = new ImparPar(7);
+            Console.WriteLine(n1.resultado);
+        }
+    }
+}
